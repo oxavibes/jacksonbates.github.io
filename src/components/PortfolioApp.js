2f4061fc011ea2356b6f5d1bd82717511c7eb113
JacksonBates <jacksonbates@hotmail.com>
2017-12-01 12:48:43 +1100

    bootstraps react modules and file structure

---
+++ src/components/PortfolioApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PortfolioApp extends React.Component {
+    state = {
+        projects: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Portfolio</h1>
+            </div>
+        )
+    }
+}
