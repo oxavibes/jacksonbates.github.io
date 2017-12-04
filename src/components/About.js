3deb28cad132e02a6a5dc18d86c97b2311b877ce
jacksonbates <malgalin@gmail.com>
2017-12-04 23:04:02 +1100

    about style in progress

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className='about'>
       <section id='about'>
-        <h1>About</h1>
-        <img src='https://avatars3.githubusercontent.com/u/4337740?s=460&v=4' alt='picture of Jackson Bates' />
+      <img className='about__img' src='https://avatars3.githubusercontent.com/u/4337740?s=460&v=4' alt='picture of Jackson Bates' />
+      <h1 className='about__name'>Jackson Bates</h1>
         <p>Web Developer</p>
       </section>
     </div>
